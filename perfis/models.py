4b4d39833b4262b8b9d427f4c21029e2897ad2b3
Tiagocf <tiago.tcf2@gmail.com>
2021-10-26 17:39:16 -0300

    Realização do módulo 6.1

--- perfis/models.py
+++ perfis/models.py
@@ -6,3 +6,14 @@ class Perfil(models.Model):
     email = models.CharField(max_length=255, null=False)
     telefone = models.CharField(max_length=15, null=False)
     nome_empresa = models.CharField(max_length=255, null=False)
+
+    def convidar(self, perfil):
+        ok = len(Convite.objects.filter(solicitante__id=self.id, convidado__id=perfil.id)) == 0
+        if(not ok):
+            print(f'Perfil de ID {self.id} já convidou perfil de ID {perfil.id}')
+            return False
+        Convite(solicitante=self, convidado=perfil).save()
+
+class Convite(models.Model):
+    solicitante = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='convites_feitos')
+    convidado = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='convites_recebidos')
--- perfis/views.py
+++ perfis/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from perfis.models import Perfil
 #from django.http import HttpResponse
 
@@ -9,3 +9,12 @@ def index(request):
 def exibir(request, perfil_id):
     perfil = Perfil.objects.get(id=perfil_id)
     return render(request, 'perfil.html', {'perfil': perfil})
+
+def convidar(request, perfil_id):
+    perfil_convidar = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_perfil_logado(request)
+    perfil_logado.convidar(perfil_convidar)
+    return redirect('index')
+
+def get_perfil_logado(request):
+    return Perfil.objects.get(id=1)
